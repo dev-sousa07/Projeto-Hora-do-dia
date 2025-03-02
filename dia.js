c9cab62652d0767210684bd5e46f5d61c6809464
sousa_mirela <sousa_mirela07@icloud.com>
2025-03-02 12:07:52 -0300

    Add files via upload

---
+++ dia.js
@@ -0,0 +1,23 @@
+var agora = new Date()
+var diasem = agora.getDate()
+
+switch(diasem){
+    case 0:
+        console.log('Segunda')
+        break
+    case 1:
+        console.log('Terça')
+    break
+    case 2:
+        console.log('Quarta')
+    break
+    case 3:
+        console.log('Quinta')
+    break
+    case 4:
+        console.log('Sexta')
+    break
+    case 5:
+        console.log('Sábado')
+    break
+}
